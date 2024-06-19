617570d355ae501f61aaa601118bd3c6b031a5f5
nguyenhoangkhue <snh110685@gmail.com>
2024-06-19 19:15:52 +0700

    upd

--- src/main/java/com/example/javaspringbootb17/controller/AdminController.java
+++ src/main/java/com/example/javaspringbootb17/controller/AdminController.java
@@ -8,12 +8,16 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.List;
+
 @Controller
 @RequiredArgsConstructor
 public class AdminController {
-    AdminService adminService;
+    private final AdminService adminService;
     @GetMapping("/admin/users")
     public String users(Model model) {
+        List<User> users=adminService.getAllUsers();
+        model.addAttribute("users",users);
         return "admin/user/index";
     }
 
@@ -25,8 +29,8 @@ public class AdminController {
     @GetMapping("/admin/users/{id}/detail")
     public String detail(@PathVariable Integer id,
                          Model model) {
-//        User user=adminService.getUserDetail(id);
-//        model.addAttribute("user",user);
+        User user=adminService.getUserDetail(id);
+        model.addAttribute("user",user);
         return "admin/user/detail";
     }
 
--- src/main/java/com/example/javaspringbootb17/service/AdminService.java
+++ src/main/java/com/example/javaspringbootb17/service/AdminService.java
@@ -5,11 +5,16 @@ import com.example.javaspringbootb17.repsitory.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class AdminService {
     private final UserRepository userRepository;
     public User getUserDetail(Integer id){
         return userRepository.findUserById(id).orElse(null);
+    }
+    public List<User> getAllUsers(){
+        return userRepository.findAll();
     }
 }
